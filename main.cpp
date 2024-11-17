d64db4d5ba56780f7bdebf384f78fb7ec6481a93
LLS-lopos <luderickls@gmail.com>
2024-11-17 14:39:09 -0400

    test

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 
 int main()
 {
-    Fenetre *fenetre = new Fenetre(1000, 800, new char[]{"LOPOS ENGIME"});
+    Fenetre *fenetre = new Fenetre(800, 800, new char[]{"LOPOS ENGIME"});
 
-    const char *vertexShaderSource = "#version 330 core\n"
+    const char *pointShaderSource = "#version 330 core\n"
                                      "layout(location = 0) in vec3 aPos;\n"
                                      "void main()\n"
                                      "{\n"
@@ -20,20 +20,24 @@ int main()
                                        "out vec4 FdfragColor;\n"
                                        "void main()\n"
                                        "{\n"
-                                       "   FdfragColor = vec4(0.0f, 0.65f, 0.0f, 1.0f);\n"
+                                       "   FdfragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
                                        "}\n";
 
-    Shader *shader = new Shader(vertexShaderSource, fragmentShaderSource);
-    Forme *tri = Forme::CreationTriangle(shader, GLDrawType::GL_DRAW_STREAM, -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f);
+    Shader *shader = new Shader(pointShaderSource, fragmentShaderSource);
+    Forme *tri = Forme::CreationTriangle(shader, GLDrawType::GL_DRAW_STREAM, 0.5);
     Forme *forme = Forme::CreationRectangle(shader, GLDrawType::GL_DRAW_STREAM, 1.0f, 0.5f);
+    Forme *poly = Forme::CreationPolygoneRegulier(shader, GLDrawType::GL_DRAW_STREAM, 50, 0.4f);
+    Forme *cercle = Forme::CreationCercle(shader, GLDrawType::GL_DRAW_STREAM, 20, 0.4f);
+    Forme *carre = Forme::CreationCarre(shader, GLDrawType::GL_DRAW_STREAM, 0.1);
+    Forme *grille = Forme::CreationGrille(shader, GLDrawType::GL_DRAW_STREAM, 15, 15);
 
     while (!glfwWindowShouldClose(fenetre->recupFenetre())) // Boucle principale
     {
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f); // Couleur de fond
         glClear(GL_COLOR_BUFFER_BIT);
 
-        forme->Draw();
-        tri->Draw();
+        grille->Draw();
+        cercle->Draw();
 
         glfwPollEvents();
         glfwSwapBuffers(fenetre->recupFenetre());
