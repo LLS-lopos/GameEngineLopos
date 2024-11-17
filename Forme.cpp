05a9327f98147dd43059286d2ffd4582c0e677e9
LLS-lopos <luderickls@gmail.com>
2024-11-17 14:38:56 -0400

    ajout création cercle, rectangle, triangle, carre, polygon régulier et option vue filaire

--- Forme.cpp
+++ Forme.cpp
@@ -1,53 +1,138 @@
 #include "Forme.h"
+#include <cmath>
 
-Forme::Forme(Shader *shader, GLDrawType glDrawType, int pointTaille, float point[], int indiceTaille, unsigned int indice[])
+Forme::Forme(Shader *shader, GLDrawType glDrawType, int pointTaille, float point[], int indiceTaille, unsigned int indice[], FormeType formeType, bool filaire)
 {
     m_shader = shader;
     m_glDrawType = glDrawType;
+    m_formeType = formeType;
+    m_filaire = filaire;
     ConfigureVBO(pointTaille, point);
     ConfigureEBO(indiceTaille, indice);
-    nombreTriangle = (int)(indiceTaille / sizeof(int));
+    n_typeForme = (int)(indiceTaille / sizeof(int));
 }
-Forme *Forme::CreationTriangle(Shader *shader, GLDrawType glDrawType,
-                               float x1, float y1, float z1,
-                               float x2, float y2, float z2,
-                               float x3, float y3, float z3)
+
+Forme *Forme::CreationRectangle(Shader *shader, GLDrawType glDrawType, float largeur, float hauteur, bool filaire)
 {
-    float point[9] = { x1, y1, z1, x2, y2, z2, x3, y3, z3 };
-    unsigned int indice[3] = { 0, 1, 2 };
-    Forme *triangle = new Forme(shader, glDrawType, sizeof(point), point, sizeof(indice), indice);
-    return triangle;
-}
-Forme *Forme::CreationRectangle(Shader *shader, GLDrawType glDrawType, float largeur, float hauteur)
-{
-    float point[12] = {-largeur / 2.0f, -hauteur / 2.0f, 0.0f,  // bas-gauche
-                        largeur / 2.0f, -hauteur / 2.0f, 0.0f,  // bas-droit
-                        largeur /2.0f, hauteur/2.0f, 0.0f,  // haut-droit
-                        -largeur / 2.0f, hauteur / 2.0f, 0.0f};  // haut-gauche
-    unsigned int indice[6] = {0,2,1,0,3,2};
-    Forme *rectangle = new Forme(shader, glDrawType, sizeof(point), point, sizeof(indice), indice);
+    float point[12] = {-largeur / 2.0f, -hauteur / 2.0f, 0.0f, // bas-gauche
+                       largeur / 2.0f, -hauteur / 2.0f, 0.0f,  // bas-droit
+                       largeur / 2.0f, hauteur / 2.0f, 0.0f,   // haut-droit
+                       -largeur / 2.0f, hauteur / 2.0f, 0.0f}; // haut-gauche
+    unsigned int indice[6] = {0, 2, 1, 0, 3, 2};
+    Forme *rectangle = new Forme(shader, glDrawType, sizeof(point), point, sizeof(indice), indice, FormeType::TRIANGLE, filaire);
     return rectangle;
+}
+Forme *Forme::CreationPolygoneRegulier(Shader *shader, GLDrawType glDrawType, int nombre_poly, float rayon, bool filaire)
+{
+    int n_point = nombre_poly * 3;
+    int nombreIndice = (nombre_poly - 2) * 3;
+    float *point = new float[n_point];
+    unsigned int *indice = new unsigned int[nombreIndice];
+
+    bool siPaire = (nombre_poly % 2) == 0;
+    float angle = 0;
+    if (siPaire)
+        angle = M_PI / 2.0f + 2.0f * M_PI / nombre_poly / 2.0f;
+    else
+        angle = M_PI / 2.0f;
+    
+    // Initialisé les points
+    for (int i = 0; i < nombre_poly; i++)
+    {
+        float x = cos(angle) * rayon;
+        float y = sin(angle) * rayon;
+
+        angle -= 2.0f * M_PI / nombre_poly;
+
+        point[i * 3] = x;
+        point[i * 3 + 1] = y;
+        point[i * 3 + 2] = 0.0f;
+    }
+    // Initialisé les indices
+    for (int triangleID = 0; triangleID < nombre_poly - 2; triangleID++)
+    {
+        indice[triangleID * 3] = 0.0f;
+        indice[triangleID * 3 + 1] = triangleID + 1;
+        indice[triangleID * 3 + 2] = triangleID + 2;
+    }
+    Forme *polygone = new Forme(shader, glDrawType, n_point * sizeof(float), point, nombreIndice * sizeof(int), indice, FormeType::TRIANGLE, filaire);
+    return polygone;    
+}
+Forme *Forme::CreationCercle(Shader *shader, GLDrawType glDrawType, int densiter, float rayon, bool filaire)
+{
+    return CreationPolygoneRegulier(shader, glDrawType, densiter, rayon, filaire);
+}
+Forme *Forme::CreationCarre(Shader *shader, GLDrawType glDrawType, float rayon, bool filaire)
+{
+    return CreationPolygoneRegulier(shader, glDrawType, 4, rayon, filaire);
+}
+Forme *Forme::CreationTriangle(Shader *shader, GLDrawType glDrawType, float rayon, bool filaire)
+{
+    return CreationPolygoneRegulier(shader, glDrawType, 3, rayon, filaire);
+}
+Forme *Forme::CreationGrille(Shader *shader, GLDrawType glDrawType, int n_carre_l, int n_carre_h, bool filaire)
+{
+    int n_point = ((n_carre_l + 1) + (n_carre_h + 1)) * 2 * 3;
+    float *point = new float[n_point];
+
+    int n_indice = ((n_carre_l + 1) + (n_carre_h + 1)) * 2;
+    unsigned int *indice = new unsigned int[n_indice];
+
+    float tailleX = 2.0f / n_carre_l;
+    for (int x = 0; x < (n_carre_l + 1); x++)
+    {
+        int index = x * 2;
+        point[index * 3] = - 1.0f;
+        point[index * 3 + 1] = - 1.0f + x * tailleX;
+        point[index * 3 + 2] = 0.0f;
+
+        point[(index + 1) * 3] = 1.0f ;
+        point[(index + 1) * 3 + 1] = -1.0f + x * tailleX;
+        point[(index + 1) * 3 + 2] = 0.0f;
+
+        indice[index] = index;
+        indice[index + 1] = index + 1;
+    }
+    float tailleY = 2.0f / n_carre_h;
+    for (int y = 0; y < (n_carre_h + 1); y++)
+    {
+        int index = (n_carre_l + 1) * 2.0f + y * 2.0f;
+        point[index * 3] = - 1.0f + y * tailleY;
+        point[index * 3 + 1] = 1.0f;
+        point[index * 3 + 2] = 0.0f;
+
+        point[(index + 1) * 3] = -1.0f + y * tailleY;
+        point[(index + 1) * 3 + 1] = -1.0f;
+        point[(index + 1) * 3 + 2] = 0.0f;
+
+        indice[index] = index;
+        indice[index + 1] = index + 1;
+    }
+    Forme *grille = new Forme(shader, glDrawType, n_point * sizeof(float), point, n_indice * sizeof(int), indice, FormeType::LIGNE, filaire);
+    return grille;
 }
 void Forme::Draw() // Dessiner la forme
 {
-    // glBindVertexArray(m_vao);
+    if (m_filaire)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
     m_shader->UtilisezShader();
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
-    //glDrawArrays(GL_TRIANGLES, 0, nombreTriangle);
-    glDrawElements(GL_TRIANGLES, nombreTriangle, GL_UNSIGNED_INT, 0);
+    glDrawElements((int)m_formeType, n_typeForme, GL_UNSIGNED_INT, 0);
+    if (m_filaire)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 void Forme::ConfigureVBO(int pointTaille, float point[])
 {
     glGenBuffers(1, &m_vbo);
     glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-    glBufferData(GL_ARRAY_BUFFER, pointTaille, point, m_glDrawType);
+    glBufferData(GL_ARRAY_BUFFER, pointTaille, point, (int)m_glDrawType);
 }
 void Forme::ConfigureEBO(int indiceTaille, unsigned int indice[])
 {
     glGenBuffers(1, &m_ebo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indiceTaille, indice, m_glDrawType);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indiceTaille, indice, (int)m_glDrawType);
 }
--- Forme.h
+++ Forme.h
@@ -3,31 +3,44 @@
 #include "Shader.h"
 #include <iostream>
 
-enum GLDrawType
+enum class GLDrawType
 {
     GL_DRAW_STREAM = 0x88E0,
     GL_DRAW_STATIC = 0x88E4,
     GL_DRAW_DYNAMIC = 0x88E8
 };
+enum class FormeType
+{
+    POINT = 0x0000,
+    LIGNE = 0x0001,
+    LIGNE_LOOP = 0x0002,
+    LIGNE_STRIP = 0x0003,
+    TRIANGLE = 0x0004,
+    TRIANGLE_STRIP = 0x0005,
+    TRIANGLE_FAN = 0x0006,
+};
 class Forme
 {
 public:
-    Forme(Shader *shader, GLDrawType glDrawType, int pointTaille, float point[], int indiceTaille, unsigned int indice[]);
-    static Forme *CreationTriangle(Shader *shader, GLDrawType glDrawType,
-                                   float x1, float y1, float z1,
-                                   float x2, float y2, float z2,
-                                   float x3, float y3, float z3);
-    static Forme *CreationRectangle(Shader *shader, GLDrawType glDrawType, float largeur, float hauteur);
+    Forme(Shader *shader, GLDrawType glDrawType, int pointTaille, float point[], int indiceTaille, unsigned int indice[], FormeType formeType = FormeType::TRIANGLE, bool filaire = false);
+    static Forme *CreationRectangle(Shader *shader, GLDrawType glDrawType, float largeur, float hauteur, bool filaire = false);
+    static Forme *CreationPolygoneRegulier(Shader *shader, GLDrawType glDrawType, int nombre_poly, float rayon, bool filaire = false);
+    static Forme *CreationCercle(Shader *shader, GLDrawType glDrawType, int densiter, float rayon, bool filaire = false);
+    static Forme *CreationCarre(Shader *shader, GLDrawType glDrawType, float rayon, bool filaire = false);
+    static Forme *CreationTriangle(Shader *shader, GLDrawType glDrawType, float rayon, bool filaire = false);
+    static Forme *CreationGrille(Shader *shader, GLDrawType glDrawType, int n_carre_l, int n_carre_h, bool filaire = false);
     void Draw();
 
 private:
     GLuint m_vbo;
     GLuint m_vao;
     GLuint m_ebo;
+    FormeType m_formeType;
     GLDrawType m_glDrawType;
     Shader *m_shader;
 
-    int nombreTriangle = 1;
+    bool m_filaire = false;
+    int n_typeForme = 1;
     void ConfigureVBO(int pointTaille, float point[]);
     void ConfigureEBO(int indiceTaille, unsigned int indice[]);
 };
